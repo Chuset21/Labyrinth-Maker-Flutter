41feaf6fe67bdc228c01d1b4250c3c022fe8a064
Miguel Bloggs <arrietam@tcd.ie>
2022-05-03 14:59:29 +0100

    Started playing around with dropdown menus

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:labyrinth_maker/board.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,18 +8,48 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Testing States in Flutter',
+      title: 'Testing dropdown menus in Flutter',
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Testing States in Flutter'),
+          title: const Text('Testing dropdown menus in Flutter'),
         ),
-        body: const Center(
-          child: Board(
-            barriers: '111001000000000000000000001000000000000000000000000000000001',
-          ),
-        ),
+        body: const InitialScreen(),
       ),
-      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class InitialScreen extends StatefulWidget {
+  const InitialScreen({Key? key}) : super(key: key);
+
+  @override
+  State<InitialScreen> createState() => _InitialScreenState();
+}
+
+class _InitialScreenState extends State<InitialScreen> {
+  final _firstListItems = ['Difficulty', 'Number of Barriers'];
+  String _firstListValue = 'Number of Barriers';
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButtonFormField(
+      elevation: 16,
+      decoration: const InputDecoration(),
+      value: _firstListValue,
+      alignment: AlignmentDirectional.center,
+      items: _firstListItems
+          .map<DropdownMenuItem<String>>(
+              (String value) => DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value, textAlign: TextAlign.center),
+                  ))
+          .toList(),
+      onChanged: (String? newValue) {
+        setState(() {
+          _firstListValue = newValue!;
+        });
+      },
     );
   }
 }
