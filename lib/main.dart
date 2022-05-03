c88bf83e9f6fcf9ec9c82b29e2a3d311e989a1b5
Miguel Bloggs <arrietam@tcd.ie>
2022-05-03 19:15:38 +0100

    Started working on routing

--- lib/main.dart
+++ lib/main.dart
@@ -67,80 +67,106 @@ class _InitialScreenState extends State<InitialScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       children: <Widget>[
         const Spacer(),
-        Container(
-          padding: const EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15.0),
-            border: Border.all(
-                color: Colors.blueAccent.shade100,
-                style: BorderStyle.solid,
-                width: 0.80),
-          ),
-          child: DropdownButton(
-            elevation: 16,
-            value: _optionValue,
-            alignment: AlignmentDirectional.center,
-            items: _optionList
-                .map<DropdownMenuItem<String>>(
-                    (String value) => DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value, textAlign: TextAlign.center),
-                        ))
-                .toList(),
-            onChanged: (String? newValue) {
-              setState(() {
-                _optionValue = newValue!;
-              });
-            },
-            underline: const SizedBox.shrink(),
+        Flexible(
+          flex: 1,
+          child: Center(
+            child: Row(
+              children: <Widget>[
+                const Spacer(),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15.0),
+                    border: Border.all(
+                        color: Colors.blueAccent.shade100,
+                        style: BorderStyle.solid,
+                        width: 0.80),
+                  ),
+                  child: DropdownButton(
+                    elevation: 16,
+                    value: _optionValue,
+                    alignment: AlignmentDirectional.center,
+                    items: _optionList
+                        .map<DropdownMenuItem<String>>((String value) =>
+                            DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value, textAlign: TextAlign.center),
+                            ))
+                        .toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _optionValue = newValue!;
+                      });
+                    },
+                    underline: const SizedBox.shrink(),
+                  ),
+                ),
+                const Spacer(),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15.0),
+                    border: Border.all(
+                        color: Colors.blueAccent.shade100,
+                        style: BorderStyle.solid,
+                        width: 0.80),
+                  ),
+                  child: DropdownButton(
+                    elevation: 16,
+                    value: _optionValue.compareTo('Difficulty') == 0
+                        ? _currentDifficulty
+                        : _currentBarrierNum.toString(),
+                    alignment: AlignmentDirectional.center,
+                    items: _optionValue.compareTo('Difficulty') == 0
+                        ? _difficulties
+                            .map((String value) => DropdownMenuItem<String>(
+                                  value: value,
+                                  child:
+                                      Text(value, textAlign: TextAlign.center),
+                                ))
+                            .toList()
+                        : _barrierNumbers
+                            .map((int value) => DropdownMenuItem<String>(
+                                  value: value.toString(),
+                                  child: Text(value.toString(),
+                                      textAlign: TextAlign.center),
+                                ))
+                            .toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        if (_optionValue.compareTo('Difficulty') == 0) {
+                          _currentDifficulty = newValue!;
+                        } else {
+                          _currentBarrierNum = int.parse(newValue!);
+                        }
+                      });
+                    },
+                    underline: const SizedBox.shrink(),
+                  ),
+                ),
+                const Spacer(),
+              ],
+            ),
           ),
         ),
-        const Spacer(),
-        Container(
-          padding: const EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15.0),
-            border: Border.all(
-                color: Colors.blueAccent.shade100,
-                style: BorderStyle.solid,
-                width: 0.80),
-          ),
-          child: DropdownButton(
-            elevation: 16,
-            value: _optionValue.compareTo('Difficulty') == 0
-                ? _currentDifficulty
-                : _currentBarrierNum.toString(),
-            alignment: AlignmentDirectional.center,
-            items: _optionValue.compareTo('Difficulty') == 0
-                ? _difficulties
-                    .map((String value) => DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value, textAlign: TextAlign.center),
-                        ))
-                    .toList()
-                : _barrierNumbers
-                    .map((int value) => DropdownMenuItem<String>(
-                          value: value.toString(),
-                          child: Text(value.toString(),
-                              textAlign: TextAlign.center),
-                        ))
-                    .toList(),
-            onChanged: (String? newValue) {
-              setState(() {
-                if (_optionValue.compareTo('Difficulty') == 0) {
-                  _currentDifficulty = newValue!;
-                } else {
-                  _currentBarrierNum = int.parse(newValue!);
-                }
-              });
-            },
-            underline: const SizedBox.shrink(),
+        Flexible(
+          flex: 1,
+          child: Center(
+            child: ElevatedButton(
+              child: const Text('Generate Board'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const MyApp()), // Change this
+                );
+              },
+            ),
           ),
         ),
-        const Spacer(),
       ],
     );
   }
