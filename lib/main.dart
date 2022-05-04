0c5f088ce4384420ec9a2d09ba505c7ae96344e4
Miguel Bloggs <arrietam@tcd.ie>
2022-05-04 16:54:21 +0100

    Changed _barrierNumbers to be more readable

--- lib/main.dart
+++ lib/main.dart
@@ -28,33 +28,7 @@ class _InitialScreenState extends State<InitialScreen> {
   String _optionValue = 'Difficulty';
 
   static const _difficulties = ['Easy', 'Medium', 'Hard', 'Very Hard'];
-  static const _barrierNumbers = [
-    0,
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10,
-    11,
-    12,
-    13,
-    14,
-    15,
-    16,
-    17,
-    18,
-    19,
-    20,
-    21,
-    22,
-    23,
-    24
-  ];
+  final _barrierNumbers = List<int>.generate(60, (index) => index);
 
   String _currentDifficulty = 'Very Hard';
   int _currentBarrierNum = 24;
